9ceb873402998e7b6dd19f89450677b278c6ee15
dikiy <dikaybulba@gmail.com>
2024-11-01 17:23:51 +0300

    add - Добавил process_table для обработки строк в таблице

--- pdf_convector.py
+++ pdf_convector.py
@@ -13,5 +13,18 @@ def extract_and_process(pdf_path):
             all_tables = page.extract_tables()
             for table in all_tables:
                 if len(table[0]) == 4:
-                    yield from table_converter(table[1:])
+                    yield from process_table(table_converter(table[1:]))
 
+def process_table(tables):
+    for row in tables:
+        for values in row:
+            if len(values) > 1:
+                if len(values[1]) < 100:
+                    values[1] = re.sub(r'(?<!\w)[0-9]{8}(?!\w)', '', values[1].upper())
+                elif 'Заработная плата' in values[1] or 'Зарплата' in values[1]:
+                    values[1] = 'ЗП'
+                if values[1] != '':
+                    yield values
+
+pdf_path = 'your_pdf_path_here.pdf'
+processed_data = list(extract_and_process(pdf_path))
