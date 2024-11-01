d2786edb27157cac3c101237fc35a2575e3c5d5e
dikiy <dikaybulba@gmail.com>
2024-11-01 18:20:22 +0300

    add - класс Database подключение к Postgres через psycopg2 и методы управления

---
+++ model.py
@@ -0,0 +1,29 @@
+import psycopg2
+import re
+class Database:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Database, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        if not hasattr(self, 'conn'):
+            self.conn = psycopg2.connect(dbname='bank_statement', user='user', password='password', host='localhost')
+            self.cursor = self.conn.cursor()
+
+    def get_cursor(self):
+        return self.cursor
+
+    def commit(self):
+        self.conn.commit()
+
+    def rollback(self):
+        self.conn.rollback()
+
+    def close(self):
+        self.cursor.close()
+
+
+
