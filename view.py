21ea15891ccd574c28b9ef832960345ad64f1e53
dikiy <dikaybulba@gmail.com>
2024-11-02 13:06:15 +0300

    add - InsertView добавил input_pdf и cancel_callback

--- view.py
+++ view.py
@@ -1,4 +1,6 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+
 from ui import Ui_Login, Ui_Form, Ui_Table
 
 class LoginView(QtWidgets.QWidget, Ui_Login):
@@ -11,3 +13,17 @@ class LoginView(QtWidgets.QWidget, Ui_Login):
     def cancel_callback(self):
         self.lineEdit.clear()
         self.lineEdit_2.clear()
+
+class InsertView(QtWidgets.QWidget, Ui_Form):
+    def __init__(self):
+        super(InsertView, self).__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.input_pdf)
+        self.buttonBox.rejected.connect(self.cancel_callback)
+
+    def input_pdf(self):
+        file, _ = QFileDialog.getOpenFileName(self, 'Save File', './', "PDF Files (*.pdf)")
+        self.lineEdit.setText(file)
+
+    def cancel_callback(self):
+        self.lineEdit.clear()
