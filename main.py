671747dff8a86c0c41bbc66468d176387ae30c90
dikiy <dikaybulba@gmail.com>
2024-11-07 10:40:02 +0300

    create - main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from PyQt5 import QtWidgets
+from model_ import AccountModel
+from view_ import Ui_Login, LoginView
+from controler import LoginController
+import sys
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    model = AccountModel()
+    view = LoginView()
+    controller = LoginController(model, view)
+    view.show()
+    sys.exit(app.exec_())
+
+import traceback
+
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+
+    text += ''.join(traceback.format_tb(tb))
+
+    QtWidgets.QMessageBox.critical(None, 'Error', text)
+
+    sys.exit()
+
+
+sys.excepthook = log_uncaught_exceptions
+if __name__ == "__main__":
+    main()
+
